c64bfbcf539ad2c5a718a911309f0d00f39ec087
Avi Levinshtein <avi.levinshtein@ironsrc.com>
2016-01-30 16:34:53 +0200

    Create player and board class

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+// Board Setup class
+var Board = function() {
+    this.width = 505; 
+    this.height = 606;
+
+    this.xslots = 6;
+    this.yslots = 6;
+};
+
+Board.prototype.getXLocation = function(x) {
+    return x * this.getXSlotSize();
+};
+
+Board.prototype.getYLocation = function(y) {
+    return y * this.getYSlotSize();
+};
+
+Board.prototype.getXSlotSize = function() {
+    return this.width / this.xslots;
+}
+
+Board.prototype.getYSlotSize = function() {
+    return this.height / this.yslots;
+}
+
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -24,12 +50,54 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    // Start location of the player
+    this.xLocation = 2;
+    this.yLocation = 5;
+    this.reset = false;
+};
 
+Player.prototype.update = function() {
+    if(this.reset === true) {
+        this.reset = false;
+        this.xLocation = 2;
+        this.yLocation = 5;
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), board.getYLocation(this.xLocation), board.getXLocation(this.yLocation) - 30);
+};
+
+Player.prototype.handleInput = function(key) {
+    if(key === 'left') {
+        if(this.xLocation > 0) {
+            this.xLocation = this.xLocation - 1; 
+        }
+    } else if (key === 'up') {
+        if(this.yLocation > 1) {
+            this.yLocation = this.yLocation - 1; 
+        }
+    } else if (key === 'right') {
+        if(this.xLocation < 4) {
+            this.xLocation = this.xLocation + 1; 
+        }
+    } else if (key === 'down') {
+        if(this.yLocation < 5) {
+            this.yLocation = this.yLocation + 1; 
+        }
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+//create the board
+var board = new Board();
+//create the player
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
